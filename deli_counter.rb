596c2f8bcd8f85116f8e696317f04c60e1bd29ff
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-09 17:58:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,12 +15,9 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.each do |person|
-    number_list = person + katz_deli.index(name) + 1
-    puts number_list
-  end
-  katz_deli.push("#{name}")
-  puts number_list
+  katz_deli << "#{name}"
+  counter +=1
+  puts "Welcome #{name}. You are number #{counter} in line."   
 end
 
 def now_serving(katz_deli)
