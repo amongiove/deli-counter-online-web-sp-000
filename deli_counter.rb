3a922b0ee885259b8f06f9cf3025d00ad67c79e4
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-09 17:28:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: " + katz_deli.join(index += 1)
+    puts "The line is currently: katz_deli.join(index += 1)"
   end
 end
 
