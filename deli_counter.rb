0b4fb2af9e1358f570987e9d007521716bf9a7e7
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-09 18:03:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,10 +15,14 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << "#{name}"
-  counter = 1
-  puts "Welcome, #{name}. You are number #{counter} in line."
-  counter += 1
+  if katz_deli.empty?
+    counter = 1
+    katz_deli << "#{name}"
+    puts "Welcome, #{name}. You are number #{counter} in line."
+  else
+    katz_deli << "#{name}"
+    counter += 1
+    puts "Welcome, #{name}. You are number #{counter} in line."
 end
 
 def now_serving(katz_deli)
