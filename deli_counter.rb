78cf9522b3dd7c04beed987b5a7800fea3ac567f
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-09 17:51:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,16 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.size == 0
+  line_position = []
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
-    position = index + 1
-    names = katz_deli.join
-    puts "The line is currently: "
+    counter = 1
+    katz_deli.each do |name|
+      line_position << "#{counter}. #{name}"
+      counter += 1  
+    end
+    puts "The line is currently: #{line_position.join(" ")}"
   end
 end
 
@@ -20,7 +24,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.size == 0
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts katz_deli[1]
