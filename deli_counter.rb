8b6ed386271463446cb514c9a74349486318230b
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-09 17:17:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty"
+  else
+    puts katz_deli.size
+  end
+end
+
+def take_a_number(katz_deli, name)
+  kaz_deli.each do |name|
+    puts "name " + katz_deli.index(name) + 1
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts katz_deli[1]
+    katz_deli.shift
+  end
+end
